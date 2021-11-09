12ddb105cc531fbcb875eb28debf0f89b452c3ec
obi3965 <46780156+obi3965@users.noreply.github.com>
2021-11-09 13:59:55 +0100

    order api is done

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,6 +1,7 @@
 const tryCatchError = require('../middleware/tryCatchError');
-const Order = require('../models/order')
-
+const Order = require('../models/order');
+const ErrorHandler = require('../util/errorHandler');
+const Product = require('../models/products')
 
 
 exports.create = tryCatchError(async(req,res,next) => {
@@ -35,41 +36,94 @@ exports.create = tryCatchError(async(req,res,next) => {
 
 
 
-exports.remove = async (req,res) => {
-    if (req.user.isAdmin) {
-        try {
-          await Order.findByIdAndDelete(req.params.id);
-          res.status(201).json("The Order has been delete...");
-        } catch (err) {
-          res.status(500).json(err);
+exports.singleOrder = tryCatchError(async(req,res,next) => {
+   
+        const singleOrder =  await Order.findById(req.params.id)
+        .populate('user', 'name email')
+        if(!singleOrder){
+          return next (new ErrorHandler('order is not found', 404))
         }
-      } else {
-        res.status(403).json("You are not allowed!");
-      }
+          res.status(200).json({
+            message:'success',
+            singleOrder
+          });     
+      
+}) 
+
+exports.allOrders = tryCatchError(async (req,res,next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+})  
+
+//all admin orders
+exports.allAdminOrders = tryCatchError(async (req,res,next) => {
+  const orders = await Order.find();
+   
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice
+  })
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+})
+
+
+//update admin orders
+exports.updateAdminOrders = tryCatchError(async (req,res,next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  if (req.body.status === "Shipped") {
+    order.orderItems.forEach(async (o) => {
+      await updateStock(o.product, o.quantity);
+    });
+  }
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+  });
+})
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.Stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
 }
 
-exports.all = async (req,res) => {
-    const typeQuery = req.query.type;
-    const genreQuery = req.query.genre;
-    let Order = [];
-    try {
-      if (typeQuery) {
-        if (genreQuery) {
-          Order = await Order.aggregate([
-            { $sample: { size: 10 } },
-            { $match: { type: typeQuery, genre: genreQuery } },
-          ]);
-        } else {
-          Order = await Order.aggregate([
-            { $sample: { size: 5 } },
-            { $match: { type: typeQuery } },
-          ]);
-        }
-      } else {
-        Order = await Order.aggregate([{ $sample: { size: 5 } }]);
-      }
-      res.status(200).json(Order);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-}
+// delete Order -- Admin
+exports.deleteOrder = tryCatchError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { create, remove, all } = require('../controllers/order');
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { create, singleOrder, allOrders,allAdminOrders,
+    updateAdminOrders } = require('../controllers/order');
+const { isAuthenticatedUser,authorizeRoles } = require('../middleware/auth');
 
 
 const router = express.Router()
 
 router.post('/new/order',isAuthenticatedUser, create)
-// router.put('/update/movies',verify, update)
- router.delete('/delete/movies/:id' , remove)
-// router.get('/find/movies/:id' ,verify, read)
-// router.get('/random/movies' ,verify, random)
- router.get('/lists', all)
+router.put('/update/orders',isAuthenticatedUser,authorizeRoles('admin'), updateAdminOrders)
+//  router.delete('/order/:id' , remove)
+ router.get('/all/orders' ,isAuthenticatedUser,authorizeRoles('admin'), allAdminOrders)
+ router.get('/order/:id', isAuthenticatedUser,singleOrder)
+ router.get('/orders/me',isAuthenticatedUser, allOrders)
 
 
 module.exports = router
